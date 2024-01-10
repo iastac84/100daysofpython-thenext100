3cefabdb34ed9f9ece0e104f572117660fca7412
iastac84 <istacey@outlook.com>
2024-01-10 11:54:33 +0000

    100daysofpython-thenext100

---
+++ 01-it-starts-with-helloworld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Day 2
+
+# Hello world with simple variable
+message = "Hello Python world!" 
+print(message) 
+
+# Changing the variable
+message = "This is another 100 days of Python! :o" 
+print(message) 
