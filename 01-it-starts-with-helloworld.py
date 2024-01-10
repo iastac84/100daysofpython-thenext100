d790b8008c68b53acf7c28db7c8fad1190f37840
iastac84 <istacey@outlook.com>
2024-01-10 12:03:30 +0000

    100daysofpython-thenext100

--- 01-it-starts-with-helloworld.py
+++ 01-it-starts-with-helloworld.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 # Day 2
-import os
+import getpass
 
-# Hello world with simple variable
-message = "Hello Python world!" 
-print(message) 
+# Hello world to current user (whoami)
+currentuser = getpass.getuser()
+print("Hello", currentuser) 
 
 # Changing the variable
 message = "This is another 100 days of Python! :o" 
 print(message) 
-
-user = os.environ.get( 'USERNAME' )
-print(user)
